1267dac6687815a548ec0d055f23ee297b244b08
Juliana <julianacarraroy@gmail.com>
2022-03-25 09:31:33 -0300

    Inicializando o repositório com os aquivos iniciais do projeto

---
+++ src/test/java/dataFactory/UsuarioDataFactory.java
@@ -0,0 +1,15 @@
+package dataFactory;
+
+import pojo.UsuarioPojo;
+
+public class UsuarioDataFactory {
+    public static UsuarioPojo criarUmUsuarioComum (){
+        UsuarioPojo usuario = new UsuarioPojo();
+        usuario.setUsuarioLogin("admin");
+        usuario.setUsuarioSenha("admin");
+
+        return usuario;
+    }
+
+
+}
